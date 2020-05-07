72ab2dbdf73a21bc621f3cd5e3fd83a98d137971
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-07 12:04:14 +0700

    BusinessCard.java was added

---
+++ business-card-java-app/src/main/java/softlab/test/task/BusinessCard.java
@@ -0,0 +1,12 @@
+package softlab.test.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessCard.class, args);
+    }
+}
