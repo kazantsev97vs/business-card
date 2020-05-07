b76acd308ca0cc8119f7426dcdeaaf51eccb74d7
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-07 12:08:48 +0700

    BaseEntity.java was added

---
+++ business-card-java-app/src/main/java/softlab/test/task/super_classes/BaseEntity.java
@@ -0,0 +1,21 @@
+package softlab.test.task.super_classes;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Каждая сущность должна иметь свой идентификатор id
+ */
+@MappedSuperclass
+@Setter @Getter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+}
