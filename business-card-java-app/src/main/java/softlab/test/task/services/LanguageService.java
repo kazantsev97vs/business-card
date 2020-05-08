c23b0e7a6bb932e3659bea824539c0689d7944e8
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-08 19:17:17 +0700

    LanguageService.java - CRUD interface added

--- business-card-java-app/src/main/java/softlab/test/task/services/LanguageService.java
+++ business-card-java-app/src/main/java/softlab/test/task/services/LanguageService.java
@@ -1,10 +1,12 @@
 package softlab.test.task.services;
 
 import softlab.test.task.entities.citizenship.Language;
+import softlab.test.task.interfaces.CRUD;
 import softlab.test.task.interfaces.SpecificBaseEntityCRUD;
 
 /**
  * CRUD-сервис для сущности "Язык"
  */
-public interface LanguageService extends SpecificBaseEntityCRUD<Language> {
+public interface LanguageService
+        extends CRUD<Language, Long>, SpecificBaseEntityCRUD<Language> {
 }
