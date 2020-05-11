feb0305e0c0a0672e08f4164f20e0999e95301c6
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-11 15:34:28 +0700

    app.js components and index.js were modified

--- business-card-react-app/src/components/app/app.js
+++ business-card-react-app/src/components/app/app.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Router from "../../router";
 
 const App = () => {
     return (
         <div className='app'>
             App
+
+            <Router/>
         </div>
     );
 };
--- business-card-react-app/src/index.js
+++ business-card-react-app/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app";
+import {Provider} from "react-redux";
+import store from "./redux/store";
+import ErrorBoundary from "./additional-components/error-boundary";
+import {AppProvider} from "./additional-components/app-context";
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+      <Provider store={store}>
+          <ErrorBoundary>
+              <AppProvider value={null}>
+                  <Router>
+                      <App/>
+                  </Router>
+              </AppProvider>
+          </ErrorBoundary>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
