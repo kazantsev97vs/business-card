4de62902fcfc2d2ec8fe34eb3ec69342bc7acbeb
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-12 12:15:06 +0700

    index.js was modified - bootstrap was added

--- business-card-react-app/src/index.js
+++ business-card-react-app/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app";
+
 import {Provider} from "react-redux";
 import store from "./redux/store";
+
 import ErrorBoundary from "./additional-components/error-boundary";
+
 import {AppProvider} from "./additional-components/app-context";
+
 import {BrowserRouter as Router} from "react-router-dom";
+
+import App from "./components/app";
+
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
