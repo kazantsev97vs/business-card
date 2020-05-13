bbb9873974480afa9b4208bd61befe307b27fbe5
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-13 22:59:42 +0700

    log-in.js route was modified

--- business-card-react-app/src/router/routes/log-in.js
+++ business-card-react-app/src/router/routes/log-in.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Route} from "react-router-dom";
+import LogInPage from "../pages/log-in";
 
 const LogInRoute = () => {
 
     return (
         <div className="log-in-route">
 
-            <Route path="/" component={LogInRoute} />
+            <Route path="/" component={LogInPage} />
 
         </div>
     );
