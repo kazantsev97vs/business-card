e14b1361e79459503fe13f6f8cd0f5ae08d124c0
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-13 22:58:11 +0700

    User.js model was modified with constructor

--- business-card-react-app/src/models/User.js
+++ business-card-react-app/src/models/User.js
@@ -21,4 +21,14 @@ export default class User {
 
     businessCardList = [];
 
+
+    constructor(firstName, lastName, login, password, email, gender, currentCity) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.currentCity = currentCity;
+    }
 }
